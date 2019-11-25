ce41625a7cdd0001bfdb6a1ced4eab0a5978e06f
Skye Geraghty <skyetay03@googlemail.com>
2019-11-25 02:46:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(arr, ele)
 {
+  return [ele, ...arr]
+}
+
+function destructivelyAddElementToEndOfArray(arr, ele)
+{
   
 }
