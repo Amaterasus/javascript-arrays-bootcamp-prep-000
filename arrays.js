c36cf3173b66c6e0374ae2ef8f8abec4e7d888ab
Skye Geraghty <skyetay03@googlemail.com>
2019-11-25 02:51:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,8 @@ function addElementToBeginningOfArray(arr, ele)
   return [ele, ...arr]
 }
 
+function destructivelyAddElementToBeginningOfArray(arr, ele)
+
 function destructivelyAddElementToEndOfArray(arr, ele)
 {
   return arr.push(ele)
